d9b67dd6cc414c335a93f4dc2be56a2762bf7e5e
Joel Berger <joel.a.berger@gmail.com>
2017-01-22 13:12:48 -0600

    add a login page

--- app.pl
+++ app.pl
@@ -3,6 +3,12 @@ use Mojolicious::Lite;
 use Mojo::File 'path';
 use Mojo::JSON qw/false true/;
 
+plugin Config => {
+  default => {
+    users => {},
+  },
+};
+
 sub _gpio { return path('/sys/class/gpio') }
 
 sub _pin {
@@ -68,9 +74,39 @@ for my $pin (keys %pins) {
 
 my $r = app->routes;
 
-$r->get('/' => 'index');
+$r->get('/login' => 'login');
 
-my $api = $r->any('/api');
+$r->post('/login' => sub {
+  my $c = shift;
+  my $users = $c->app->config->{users};
+  my $user = $c->param('username');
+  if (my $pass = $users->{$user}) {
+    if ($c->param('password') eq $pass) {
+      $c->session->{username} = $user;
+      return $c->redirect_to('index');
+    }
+  }
+  $c->render('login');
+});
+
+$r->get('/logout' => sub {
+  my $c = shift;
+  $c->session->{expires} = 1;
+  $c->redirect_to('login');
+});
+
+my $auth = $r->under('/' => sub {
+  my $c = shift;
+
+  return 1 if $c->session->{username};
+
+  $c->redirect_to('login');
+  return 0;
+});
+
+$auth->get('/' => 'index');
+
+my $api = $auth->any('/api');
 
 my $door = $api->any('/door');
 
@@ -158,3 +194,124 @@ __DATA__
   </script>
 </body>
 </html>
+
+@@ login.html.ep
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- style adapted from http://codepen.io/larrygeams/pen/Itear  -->
+  %= stylesheet begin
+    html, body {
+      width: 100%;
+      height: 100%;
+      margin: 0px;
+    }
+
+    .container {
+      width: 100%;
+      height: 100%;
+      margin-top: 20px;
+      display: flex;
+      justify-content: center;
+    }
+
+    .form{
+      max-width: 90%;
+      width: 400px;
+      height: 230px;
+      background: #edeff1;
+      margin: 0px auto;
+      padding-top: 20px;
+      border-radius: 10px;
+      -moz-border-radius: 10px;
+      -webkit-border-radius: 10px;
+    }
+
+    input[type="text"], input[type="password"]{
+      display: block;
+      width: 80%;
+      height: 35px;
+      margin: 15px auto;
+      background: #fff;
+      border: 0px;
+      padding: 5px;
+      font-size: 16px;
+      border: 2px solid #fff;
+      transition: all 0.3s ease;
+      border-radius: 5px;
+      -moz-border-radius: 5px;
+      -webkit-border-radius: 5px;
+    }
+
+    input[type="text"]:focus, input[type="password"]:focus{
+      border: 2px solid #1abc9d
+    }
+
+    input[type="submit"]{
+      display: block;
+      background: #1abc9d;
+      width: 80%;
+      padding: 12px;
+      cursor: pointer;
+      color: #fff;
+      border: 0px;
+      margin: auto;
+      border-radius: 5px;
+      -moz-border-radius: 5px;
+      -webkit-border-radius: 5px;
+      font-size: 17px;
+      transition: all 0.3s ease;
+    }
+
+    input[type="submit"]:hover{
+      background: #09cca6
+    }
+
+    a{
+      text-align: center;
+      font-family: Arial;
+      color: gray;
+      display: block;
+      margin: 15px auto;
+      text-decoration: none;
+      transition: all 0.3s ease;
+      font-size: 12px;
+    }
+
+    a:hover{
+      color: #1abc9d;
+    }
+
+    ::-webkit-input-placeholder {
+      color: gray;
+    }
+
+    :-moz-placeholder { /* Firefox 18- */
+      color: gray;
+    }
+
+    ::-moz-placeholder {  /* Firefox 19+ */
+      color: gray;
+    }
+
+    :-ms-input-placeholder {
+      color: gray;
+    }
+  % end
+</head>
+<body>
+<div class="container">
+  <div class="form">
+    %= form_for login => (method => 'POST') => begin
+      <input type="text" name="username" placeholder="Username">
+      <input type="password" name="password" placeholder="Password">
+      <input type="submit" value="Login">
+      <!-- <a href="">Lost your password?</a> -->
+    % end
+  </div>
+</div>
+</body>
+</html>
+
